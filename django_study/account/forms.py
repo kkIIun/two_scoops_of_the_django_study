a1a7e8258a04bcecf3b4a4e5c48b6f6b8f2fa214
leekijung <rlwjd4177@naver.com>
2021-01-19 18:47:36 +0900

    14,15장: 장고 탬플릿

--- django_study/account/forms.py
+++ django_study/account/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import CustomUser
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth.hashers import check_password
-
+from django.contrib.auth.models import User
 
 class SignInForm(forms.Form):
     username = forms.CharField(max_length=32, label="아이디",
@@ -20,7 +20,7 @@ class SignInForm(forms.Form):
         password = cleaned_data.get('password')
         
         if username and password :
-            user = CustomUser.objects.get(username=username)
+            user = User.objects.get(username=username)
 
             if not check_password(password,user.password) :
                 self.add_error('password','비밀번호가 틀렸습니다.')
@@ -28,21 +28,21 @@ class SignInForm(forms.Form):
         return cleaned_data
 
 
-class SignUpForm(forms.ModelForm) :
+class SignUpForm(UserCreationForm) :
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['phone'].required = True
-        self.fields['address'].required = True
+        # self.fields['phone'].required = True
+        # self.fields['address'].required = True
 
     class Meta:
-        model = CustomUser
-        fields = ['username','password','password_confirm','phone','address']
+        model = User
+        fields = ['username','password1','password2']
 
     def clean(self):
         cleaned_data = super().clean()
-        password1 = cleaned_data.get('password')
-        password2 = cleaned_data.get('password_confirm')
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
 
         if password1 != password2 :
             self.add_error('password_confirm','비밀번호가 일치하지 않습니다.')
--- django_study/account/models.py
+++ django_study/account/models.py
@@ -4,5 +4,4 @@ from django.contrib.auth.models import User
 class CustomUser(User):
     phone = models.CharField(max_length=20, blank=True)
     address = models.CharField(max_length=100, blank=True)
-    password_confirm = models.CharField(max_length=20)
 # Create your models here.
